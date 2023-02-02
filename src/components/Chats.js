7bc8e69d87eeaf930dec8786bdab88a05543cf5b
MayceAlazzawi <maynab4@gmail.com>
2023-02-02 22:09:53 +0300

    commenting chats

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -36,7 +36,7 @@ const Chats = () => {
   };
   return (
     <div>
-      {Object.entries(chats)
+      {/* {Object.entries(chats)
         ?.sort((a, b) => b[1].date - a[1].date)
         .map((chat) => (
           <div
@@ -58,7 +58,7 @@ const Chats = () => {
               </p>
             </div>
           </div>
-        ))}
+        ))} */}
     </div>
   );
 };
